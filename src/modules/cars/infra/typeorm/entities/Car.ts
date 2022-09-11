349b4e3718aa491ea40ea53b4810f9314e6355c4
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-09-11 20:10:31 -0300

    Erro de xxx.avaliable not supported in postgres

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,22 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+import { Category } from "./Category";
+
+@Entity("cars")
 class Car {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   name!: string;
+  @Column()
   description!: string;
+  @Column()
   daily_rate!: number;
-  avaliable!: boolean;
+  @Column()
+  avaliable = true;
+  @Column()
   license_plate!: string;
+  @Column()
   fine_amount?: number;
+  @Column()
   brand!: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: "category_id" })
+  category!: Category;
+  @Column()
   category_id?: string;
+  @CreateDateColumn()
   created_at!: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidv4();
-      this.avaliable = true;
-      this.created_at = new Date();
     }
   }
 }
