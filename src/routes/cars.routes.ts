c017953a8454f3b22ccc0bdc6e0ac7cb4800d1ec
Antonio Alves <antonioaugusto.alves1981@gmail.com>
2022-09-17 16:12:08 -0300

    Criando o middleware de administração

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
+import { ensureAdmin } from "middlewares/ensureAdmin";
+import { ensureAuthetication } from "middlewares/ensureAuthentication";
 
 import { CreateCarController } from "@modules/cars/useCases/createCars/CreateCarController";
 
 const carsRoutes = Router();
 const createCarController = new CreateCarController();
 
-carsRoutes.post("/", createCarController.handle);
+carsRoutes.post(
+  "/",
+  ensureAuthetication,
+  ensureAdmin,
+  createCarController.handle
+);
 
 export { carsRoutes };
